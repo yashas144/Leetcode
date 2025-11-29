b0602bec5703854e2cfb85734250877828475227
Yashas Chandra <111414758+yashas144@users.noreply.github.com>
2025-11-28 21:54:41 -0600

    Add files via upload

---
+++ Min_Ops_To_Make_Array_Sum_Div_By_K.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def minOperations(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        S = sum(nums)
+        r = S % k
+        return r
